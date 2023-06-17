6895abe2bf36288383a88953674e0f9d7a9e3040
PyroTech-lab <105421457+PyroTech-lab@users.noreply.github.com>
2023-06-17 22:37:01 +0200

    Add files via upload

--- actions/users/addVerificationsAction.php
+++ actions/users/addVerificationsAction.php
@@ -1,6 +1,11 @@
 <?php
-
+use PHPMailer\PHPMailer\PHPMailer;
 require('actions/database.php');
+
+
+
+$EnterCodeBoxDisplay = "none";
+
 
 
 if(isset($_POST['phone_submit'])){
@@ -44,4 +49,75 @@ if(isset($_POST['idcard_submit'])){
 			header("Refresh:0");
 				}
 				
+
+
+
+
+
+
+if(isset($_POST['email_verify'])){
+
+$user_email = $_SESSION['email'];
+
+$random_number = ''; for ($i = 0; $i<6; $i++) {$random_number .= mt_rand(0,9);}
+
+$insertCodeinData = $bdd->prepare('UPDATE users  SET verification_code= ? WHERE id=?');
+$insertCodeinData->execute(array($random_number, $id_user));
+
+require_once 'vendor/autoload.php';
+
+$phpmailer = new PHPMailer();
+$phpmailer->isSMTP();
+$phpmailer->Host = 'live.smtp.mailtrap.io';
+$phpmailer->SMTPAuth = true;
+$phpmailer->Port = 587;
+$phpmailer->Username = 'api';
+$phpmailer->Password = '80c05e0ef1f980aa713b7b0a91f9113e';
+
+$phpmailer->setFrom('contact@star-agency.digital','Instant Borrow');
+$phpmailer->addAddress(''.$user_email.'');
+$phpmailer->Subject = 'Instant Borrow Email Verification Code';
+
+$phpmailer->Body = '<html><span>'.$random_number.'</span></br><span>Is your Instant Borrow Verification Code.</span></html>';
+$phpmailer->AltBody = ''.$random_number.' Is your Instant Borrow verification Code.';
+
+
+if (!$phpmailer->send()) {
+	$mail_faillure = "Email Error. Please Try Again.";
+}else{
+	$mail_success = "Verifcation Email Sent.";
+	$EnterCodeBoxDisplay = "block";
+}
+
+
+
+	if(isset($_POST['email_verifcation_submit'])){
+
+		if(isset($_POST['email_verifcation_code'])){
+	
+			$entered_email_code = $_POST['email_verifcation_code'];
+
+			$GetCode = $bdd->prepare('SELECT  verification_code FROM users WHERE id=?');
+			$GetCode->execute(array($id_user));
+
+			$CodeInfos = $GetCode->fetch();
+
+			if($entered_email_code = '.$CodeInfos["verification_code"].'){
 				
+				
+				$insertVerifiedinData = $bdd->prepare('UPDATE users  SET verification_code="" email_verified="yes" WHERE id=?');
+				$insertVerifiedinData->execute(array($id_user));
+				
+				$EnterCodeBoxDisplay = "none";
+				
+				$email_verified_message = "Email verified!";
+				
+			}else{
+				$email_notverified = "Wrong Verification Code.";
+			}
+
+		}
+
+	}
+
+}
