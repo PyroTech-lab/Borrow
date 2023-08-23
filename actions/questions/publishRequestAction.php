0972b0f70881c5e103e3c1d513b51e7ebf7f064b
PyroTech-lab <105421457+PyroTech-lab@users.noreply.github.com>
2023-08-23 12:30:28 +0200

    Add files via upload

--- actions/questions/publishRequestAction.php
+++ actions/questions/publishRequestAction.php
@@ -102,12 +102,24 @@ if(isset($_POST['submit'])){
 														}
 														
 														
-														if (strlen($idcard_verified) == 0) {
-															$idcard = "0";
-														
-														}else{
-															$idcard = "20";
-														}
+															if (strlen($idcard_verified) == 0) {
+																$idcard = "0";
+															
+															}else{
+																$CheckIDVerified = $bdd->prepare('SELECT id_verified FROM users WHERE id = ?');
+																$CheckIDVerified->execute(array($_SESSION['id']));
+																
+																$row311 = $CheckIDVerified->fetch(PDO::FETCH_ASSOC);
+																$verified_orNot311 = $row311['id_verified'];
+																
+																if($verified_orNot311 == "verified"){
+																$idcard = "20";
+																}
+																
+																if($verified_orNot311 == "under_verification"){
+																$idcard = "0";
+																}
+															}
 														
 														
 														
