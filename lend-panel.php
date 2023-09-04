d5f6992fef55cbccdaed1531453975273183ff5a
PyroTech-lab <105421457+PyroTech-lab@users.noreply.github.com>
2023-09-04 10:36:57 +0200

    Update lend-panel.php

--- lend-panel.php
+++ lend-panel.php
@@ -18,7 +18,9 @@ require('actions/users/bannedAction.php');
 <head>
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+<meta charset="UTF-8">
+<meta name="robots" content="index" />
+	
 <title>Lend Money to <?= $username_borrower; ?> - Instant Borrow</title>
 
 <!-- icons generated with https://favicomatic.com/ -->
