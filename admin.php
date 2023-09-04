c26a850e662550a5a818c7b3f3b1956aac6b331e
PyroTech-lab <105421457+PyroTech-lab@users.noreply.github.com>
2023-09-04 10:19:36 +0200

    Update admin.php

--- admin.php
+++ admin.php
@@ -17,7 +17,9 @@ if(!isset($_SESSION['admin'])){
 <head>
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+<meta charset="UTF-8">
+<meta name="robots" content="noindex" />
+	
 <title>Administrator - Instant Borrow</title>
 
 <!-- icons generated with https://favicomatic.com/ -->
