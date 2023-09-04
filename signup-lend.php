717c787701b363f9b91c2fbf5f14f8d8314ce827
PyroTech-lab <105421457+PyroTech-lab@users.noreply.github.com>
2023-09-04 11:43:46 +0200

    Update signup-lend.php

--- signup-lend.php
+++ signup-lend.php
@@ -17,7 +17,9 @@ if(isset($_SESSION['auth'])){
 <head>
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+<meta charset="UTF-8">
+<meta name="robots" content="noindex" />
+	
 <title>Sign Up - Instant Borrow</title>
 
 <!-- icons generated with https://favicomatic.com/ -->
