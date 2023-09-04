25d262936749ab183ae5bdf322d63fd9238ad6af
PyroTech-lab <105421457+PyroTech-lab@users.noreply.github.com>
2023-09-04 11:34:55 +0200

    Update privacy-policy-loggedin.php

--- privacy-policy-loggedin.php
+++ privacy-policy-loggedin.php
@@ -10,7 +10,9 @@ require('actions/users/securityAction.php');
 <head>
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+<meta charset="UTF-8">
+<meta name="robots" content="noindex" />
+	
 <title>Privacy Policy - Instant Borrow</title>
 
 <!-- icons generated with https://favicomatic.com/ -->
