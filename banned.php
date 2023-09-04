19c3230cc5b0a51a67ca492ddc827c918aec6b19
PyroTech-lab <105421457+PyroTech-lab@users.noreply.github.com>
2023-09-04 10:20:57 +0200

    Update banned.php

--- banned.php
+++ banned.php
@@ -28,7 +28,9 @@ if(!isset($_SESSION['banned'])AND(isset($_SESSION['auth']))){
 <head>
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+<meta charset="UTF-8">
+<meta name="robots" content="noindex" />
+	
 <title>Account Suspended - Instant Borrow</title>
 
 <!-- icons generated with https://favicomatic.com/ -->
