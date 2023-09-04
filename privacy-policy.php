5f782a528ed69d9c1b9cd4319ddc8d0725f06b8a
PyroTech-lab <105421457+PyroTech-lab@users.noreply.github.com>
2023-09-04 11:36:17 +0200

    Update privacy-policy.php

--- privacy-policy.php
+++ privacy-policy.php
@@ -9,8 +9,10 @@ require('actions/questions/updateDatabases.php');
 <head>
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="UTF-8">
+<meta name="robots" content="index" />
 
-<title>Privacy Policy - Instant Borrow</title>
+	<title>Privacy Policy - Instant Borrow</title>
 
 <!-- icons generated with https://favicomatic.com/ -->
 <link rel="apple-touch-icon-precomposed" sizes="57x57" href="assets/images/pageicons/apple-touch-icon-57x57.png" />
