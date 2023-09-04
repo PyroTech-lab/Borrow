3a4591ebc6237f9749528589461b96784b63b116
PyroTech-lab <105421457+PyroTech-lab@users.noreply.github.com>
2023-09-04 11:55:10 +0200

    Update wall-of-shame.php

--- wall-of-shame.php
+++ wall-of-shame.php
@@ -10,7 +10,9 @@ require('actions/questions/updateDatabases.php');
 <head>
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+<meta charset="UTF-8">
+<meta name="robots" content="index" /> 
+	
 <titleWall of Shame - Instant Borrow</title>
 
 <!-- icons generated with https://favicomatic.com/ -->
