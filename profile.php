291788ce7954ec33cae6a79859c5401a878f3a5b
PyroTech-lab <105421457+PyroTech-lab@users.noreply.github.com>
2023-09-04 11:37:58 +0200

    Update profile.php

--- profile.php
+++ profile.php
@@ -20,7 +20,9 @@ require('actions/users/bannedAction.php');
 <head>
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+<meta charset="UTF-8">
+<meta name="robots" content="noindex" />
+	
 <title>Your profile - Instant Borrow</title>
 
 <!-- icons generated with https://favicomatic.com/ -->
