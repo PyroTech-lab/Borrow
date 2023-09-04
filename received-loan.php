e6139636ea39838c65261989b64152f88687885e
PyroTech-lab <105421457+PyroTech-lab@users.noreply.github.com>
2023-09-04 11:39:16 +0200

    Update received-loan.php

--- received-loan.php
+++ received-loan.php
@@ -18,7 +18,9 @@ require('actions/users/bannedAction.php');
 <head>
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+<meta charset="UTF-8">
+<meta name="robots" content="noindex" />
+	
 <title><?= $loan_amount; ?>$ Loan Received from <?= $username_lender; ?> - Instant Borrow</title>
 
 <!-- icons generated with https://favicomatic.com/ -->
