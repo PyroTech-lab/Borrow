0df3d83728c815760f761bb96c58eeb229da797b
PyroTech-lab <105421457+PyroTech-lab@users.noreply.github.com>
2023-09-04 10:26:21 +0200

    Update borrow-yeslogin.php

--- borrow-yeslogin.php
+++ borrow-yeslogin.php
@@ -13,7 +13,9 @@ require('actions/users/bannedAction.php');
 <head>
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+<meta charset="UTF-8">
+<meta name="robots" content="noindex" />
+	
 <title>Instant Borrow - Borrow Money Quickly and Easily</title>
 
 <!-- icons generated with https://favicomatic.com/ -->
