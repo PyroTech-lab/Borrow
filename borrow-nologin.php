e02c7a43766710f4ce10607189bfe68bf64cd32e
PyroTech-lab <105421457+PyroTech-lab@users.noreply.github.com>
2023-09-04 10:25:43 +0200

    Update borrow-nologin.php

--- borrow-nologin.php
+++ borrow-nologin.php
@@ -16,7 +16,9 @@ if(isset($_SESSION['auth'])){
 <head>
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+<meta charset="UTF-8">
+<meta name="robots" content="index" />
+	
 <title>Instant Borrow - Borrow Money Quickly and Easily</title>
 
 <!-- icons generated with https://favicomatic.com/ -->
