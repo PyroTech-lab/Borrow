e93fd630f6a24193b4c968c623a25e14628e153e
PyroTech-lab <105421457+PyroTech-lab@users.noreply.github.com>
2023-09-04 10:39:55 +0200

    Update loan-nologin.php

--- loan-nologin.php
+++ loan-nologin.php
@@ -26,7 +26,9 @@ if(isset($_SESSION['auth'])){
 <head>
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+<meta charset="UTF-8">
+<meta name="robots" content="index" />
+	
 <title>Lend Money to <?= $username_borrower; ?> - Instant Borrow</title>
 
 <!-- icons generated with https://favicomatic.com/ -->
